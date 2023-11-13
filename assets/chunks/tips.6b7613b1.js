81bb1ca34064702f59381d5971d2de56cd311d9a
XPoet <XPoet@users.noreply.github.com>
2023-11-13 09:54:27 +0000

    Deploy Keep Theme Documents (v3) theme-keep/docs-site-source@e390d27e0e106265ef94d5d1b359c547eed5fc35

---
+++ assets/chunks/tips.6b7613b1.js
@@ -0,0 +1,1 @@
+import{_,a as s,S as c}from"./framework.6db2b512.js";const e={},t={class:"tips"};function a(n,o){return s(),c("div",t," 配置指南的目录结构跟主题配置文件 _config.yml 的配置项保持一一对应，方便查看。 ")}const i=_(e,[["render",a],["__scopeId","data-v-acd46765"]]);export{i as T};
